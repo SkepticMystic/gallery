41d53d8845b9b468bb1a90b17f05ac4ac861be1a
SkepticMystic <rossk29@gmail.com>
2025-12-15 07:02:21 +0200

    Update sitemap

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,26 +1,45 @@
+import type { RouteId } from "$app/types";
 import { APP } from "$lib/const/app.const";
-import type { RequestHandler } from "@sveltejs/kit";
+import { ERROR } from "$lib/const/error.const";
+import { db } from "$lib/server/db/drizzle.db";
+import { Repo } from "$lib/server/db/repos/index.repo";
+import { error, type RequestHandler } from "@sveltejs/kit";
 import * as sitemap from "super-sitemap";
 
 export const prerender = true;
 
 export const GET: RequestHandler = async () => {
-  //   const [tasks] = await Promise.all([
-  //     db.query.task.findMany({
-  //       columns: { id: true, updatedAt: true },
-  //     }),
-  //   ]);
+  const [galleries, pieces] = await Promise.all([
+    Repo.query(
+      db.query.gallery.findMany({
+        columns: { slug: true, updatedAt: true },
+      }),
+    ),
+    Repo.query(
+      db.query.piece.findMany({
+        columns: { slug: true, updatedAt: true },
+      }),
+    ),
+  ]);
+
+  if (!galleries.ok || !pieces.ok) {
+    error(500, ERROR.INTERNAL_SERVER_ERROR);
+  }
 
   return await sitemap.response({
     origin: APP.URL,
 
-    excludeRoutePatterns: ["^/admin", "^/tasks/\\[id\\]"],
+    excludeRoutePatterns: ["^/admin", "^/s/"],
 
-    // paramValues: {
-    //   "/tasks/[task_id]": tasks.map((task) => ({
-    //     values: [task.id],
-    //     lastmod: task.updatedAt?.toISOString().split("T")[0],
-    //   })),
-    // } satisfies Partial<Record<RouteId, sitemap.ParamValues[string]>>,
+    paramValues: {
+      "/gallery/[slug]": galleries.data.map((d) => ({
+        values: [d.slug],
+        lastmod: d.updatedAt?.toISOString().split("T")[0],
+      })),
+      "/piece/[slug]": pieces.data.map((d) => ({
+        values: [d.slug],
+        lastmod: d.updatedAt?.toISOString().split("T")[0],
+      })),
+    } satisfies Partial<Record<RouteId, sitemap.ParamValues[string]>>,
   });
 };
